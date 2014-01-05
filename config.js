88f3849b9f2ac676c99ba784fe79d4e3a467d696
Mehfuz Hossain <mehfuz@gmail.com>
2014-01-05 13:06:45 -0600

    grunt file

---
+++ config.js
@@ -0,0 +1,53 @@
+(function() {
+  var config, express, util;
+
+  express = require('express');
+
+  util = require('util');
+
+  config = function(app) {
+    return app.configure(function() {
+      app.set('port', process.env.PORT || 3000);
+      app.set('views', __dirname + '/views');
+      app.set('view engine', 'jade');
+      app.use(express.favicon(__dirname + '/public/favicon.ico'));
+      app.use(express.logger('dev'));
+      app.use(function(req, res, next) {
+        var data, length,
+          _this = this;
+        app.host = util.format('http://%s/', req.headers['host']);
+        data = [];
+        length = 0;
+        req.on('data', function(chunk) {
+          length += chunk.length;
+          data.push(chunk);
+        });
+        return req.on('end', function() {
+          req.rawBody = Buffer.concat(data, length);
+          next();
+        });
+      });
+      app.use(app.router);
+      app.use(express.bodyParser());
+      app.use(express.methodOverride());
+      app.use(require('less-middleware')({
+        src: __dirname + '/public'
+      }));
+      app.use(express.compress());
+      if (process.env.NODE_ENV === 'production') {
+        app.use(express["static"](__dirname + '/public', {
+          maxAge: 86400000
+        }));
+      } else {
+        app.use(express["static"](__dirname + '/public'));
+      }
+      app.locals.pretty = true;
+      return app.configure('production', function() {
+        return app.use(express.errorHandler());
+      });
+    });
+  };
+
+  module.exports = config;
+
+}).call(this);
---
+++ settings.coffee
@@ -0,0 +1,31 @@
+module.exports = ()->
+	class Settings
+		constructor:(app) ->
+			@mongoose = require('mongoose')
+			@Promise = require('node-promise').Promise  
+			@GridStore = require('mongodb').GridStore
+			# init mongo        
+			url = process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/lighter'
+			@mongoose.connect url
+			@marked = require('marked')			
+			@marked.setOptions
+				highlight:(code,lang) ->
+					hl = require('highlight.js')
+					hl.tabReplace = '    '
+					(hl.highlightAuto code).value
+					
+		marked		:	@marked
+		mongoose	:	@mongoose
+		url			:	'/'
+		author 		:	process.env.AUTHOR || 'Editor'
+		title		:	process.env.BLOG_TITLE || 'Lighter Blog'
+		username	:	process.env.USER || 'admin'
+		password	:	process.env.PASSWORD || 'admin' 
+		feedUrl		:	process.env.FEED_URL || null
+		updated		:	new Date()
+		engine		:	'Lighter Blog Engine'
+		format: (content) ->
+		  @marked(content)
+						
+	new Settings()
+	
